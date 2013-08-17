cc3df2b23e9b190a54d34932a7e38201c1e66159
jaredstander <jaredstander@gmail.com>
2013-08-16 22:15:40 -0700

    label model validation tests pass

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,6 +1,8 @@
 class Label < ActiveRecord::Base
 	belongs_to :form
 	belongs_to :field
-	attr_accessible :form_id, :field_id, :name
+	attr_accessible :field_id, :name
   # attr_accessible :title, :body
+
+  validates :form_id, :field_id, :name, presence: true
 end
