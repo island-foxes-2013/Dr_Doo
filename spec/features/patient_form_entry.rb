7fb7650a59f0fa4840d9fc563ea0a34c6decf5b5
Apprentice <apprentice@devbootcamp.com>
2013-08-17 13:44:36 -0700

    forms_display rebase

--- spec/features/patient_form_entry.rb
+++
@@ -1,9 +0,0 @@
- require 'spec_helper'
- 
-  scenario "as a Patient I want to input information into a field" do
-   pending
-  end
-
-  scenario "as a Patient I want to send an encrypted form to my Provider" do
-   pending
-  end
