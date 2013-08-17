10bcf3e738716ce6808fc02cf2373e11f72d2ee3
jaredstander <jaredstander@gmail.com>
2013-08-17 13:28:55 -0700

    pre-origin pull commit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,50 @@ require 'spec_helper'
 
 describe User do
   context "#new" do
-    it "has a test" do
-      # test
+    it "allows mass-assignment of email" do
+      expect{ user = User.new(email: "example@email.com") }.not_to raise_error
     end
+
+    it "allows mass-assignment of password" do
+      expect{ user = User.new(password: "password") }.not_to raise_error
+    end
+
+    it { should respond_to(:email) }
   end
 
   context "#save" do
-    it "has a test" do
-      # test
+    it "requires a valid email" do
+      user = User.new(password: "password", password_confirmation: "password")
+      expect(:user).not_to be_valid
+    end
+
+    it "requires a valid password" do
+      user = User.new(email: "email@email.com", password_confirmation: "password")
+      expect(:user).not_to be_valid
+    end
+
+    it "requires a password validation" do
+      user = User.new(password: "password", password_confirmation: "password")
+      emails = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      emails.each do |invalid_email|
+        user.email = invalid_email
+        user.should be_invalid
+      end
     end
   end
 end
+
+# Add in email format validation
+# it "should be invalid" do
+#       emails = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+#       emails.each do |invalid_email|
+#         user.email = invalid_email
+#         user.should be_invalid
+#       end
+#     end
+
+# TODO: Add in password format validation
+# describe "with a password that's too short" do
+#     let(:user) { user.password = @user.password_confirmation = "a" * 5 }
+#     it { should be_invalid }
+#   end
