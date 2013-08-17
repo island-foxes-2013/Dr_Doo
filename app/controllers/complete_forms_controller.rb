35ea76626046fcf6f59bcc15ec26e2ac5acdf623
Apprentice <apprentice@devbootcamp.com>
2013-08-17 13:49:46 -0700

    revised complete_forms routes

--- app/controllers/complete_forms_controller.rb
+++ app/controllers/complete_forms_controller.rb
@@ -1,2 +1,19 @@
 class CompleteFormsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+  
+  def edit
+  end
+
+  def put
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ DrDoo::Application.routes.draw do
   devise_for :users 
 
   resources :forms
-  resources :complete_forms, only: [:new, :create, :show, :edit, :delete]
+  resources :complete_forms, except: [:index]
   root to: 'home#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
