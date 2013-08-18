9fe7dbd05dda23151f5e9c49131c0912bb176bb4
jaredstander <jaredstander@gmail.com>
2013-08-17 18:42:25 -0700

    added notification method and method view for actionmailer

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,3 +1,9 @@
 class Notifications < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "notifications@drdoo.com"
+
+  def new_form_to_complete(user)
+    @user = user
+    @url = "New form's URL"
+    mail(to: @user.email, subject: "#{@user.name}, you have a new form to complete! Cats!")
+  end
 end
