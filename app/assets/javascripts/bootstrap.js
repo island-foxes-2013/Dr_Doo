f98fcb0c1e3d443ad12593aa654be4401b8330ef
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-08-19 11:57:53 -0700

    reorganizing JS files for clarity

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,3 +1,5 @@
+// TODO-JW: this belongs in vendor/assets
+
 /* ===================================================
  * bootstrap-transition.js v2.3.2
  * http://getbootstrap.com/2.3.2/javascript.html#transitions
