96a549ab16d08e60683d9b69d5a238281c72ece4
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-08-19 12:05:20 -0700

    move bootstrap stuff into vendor/assets

--- vendor/assets/javascripts/bootstrap.js
+++ vendor/assets/javascripts/bootstrap.js
@@ -1,5 +1,3 @@
-// TODO-JW: this belongs in vendor/assets
-
 /* ===================================================
  * bootstrap-transition.js v2.3.2
  * http://getbootstrap.com/2.3.2/javascript.html#transitions
