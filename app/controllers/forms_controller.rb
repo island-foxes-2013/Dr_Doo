95c67b9886b7b93508a1c896addef1b933f5879c
Apprentice <apprentice@devbootcamp.com>
2013-08-20 11:07:24 -0700

    working empty forms now this should now be ready to fill in

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -12,9 +12,7 @@ class FormsController < ApplicationController
 	end
 
   def show
-    # user = User.find(1)
-    # @form = user.forms.first
-    # @form_answers = @form.answers
+    @form = Form.find(params[:id])
   end
 
   def new
@@ -24,41 +22,14 @@ class FormsController < ApplicationController
   end
 
   def create_or_update_contact_info
-    params inspect
   end
 
   def create
-      p 'here I am _____________________________________'
-      p params[:form]
-      p params[:form][:title]
-
-    user = User.find(1)
-    @form = user.forms.create(params[:form])
-
-     p params[:fields]['element_id']
-
-     p params[:fields]['element_label']
-
+    @form = current_user.forms.create(params[:form])
     params[:fields].each do |field|
-      p field
-      # @form.fields.create(field)
-    end
+      element = Element.find(field[:element_id])
+      @form.fields.create(element_id: element.id, label: element.label)
+    end 
     redirect_to form_path(@form)
-    # render json: { comment: "success" }
-
-
-    # if @form.save
-    #   # TODO-JW: do something more meaningful here
-    #   render text: params.to_json
-    # else
-    #   # TODO-JW: do something more meaningful here
-    #   render text: "ERROR!"
-    # end
-    # user = User.first
-    # form = user.forms.create(params[:form])
-    # params[:fields].each do |field|
-    #   form.fields.create(element_id: field[:element_id], name: field[:name] )
-    # end
   end
-
 end
