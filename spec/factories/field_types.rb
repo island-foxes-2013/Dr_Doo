3d42379e725af570a55986198bfb978f56f8b82b
jaredstander <jaredstander@gmail.com>
2013-08-19 17:40:10 -0700

    continued removing unnecessary spec and factory files

--- spec/factories/field_types.rb
+++
@@ -1,6 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :field_type do
-  end
-end
--- spec/factories/integeranswer.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-
-  factory :integeranswer do
-    value { 242 }
-  end
-end
