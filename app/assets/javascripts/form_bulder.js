6fa4097417a35cea67d8447e24a9d68bea5dac57
Apprentice <apprentice@devbootcamp.com>
2013-08-19 18:25:40 -0700

    forms builder is done

--- app/assets/javascripts/form_bulder.js
+++ app/assets/javascripts/form_bulder.js
@@ -1,117 +1,40 @@
 $(function() {
-  // $('.field').draggable();
-  // var $elem = $('')
-  // $('.name_field').draggable();
-
- // $(".gridster ul").gridster({
- //        widget_margins: [10, 10],
- //        widget_base_dimensions: [140, 140]
- //    });
-
- //   var gridster = $(".gridster li").gridster().data('gridster');
-
-// gridster.add_widget('<li class="new">The HTML of the widget...</li>', 2, 1);
-
-
-
-// var dropped = false;
-
- // $(".item").draggable({
-     
- //     helper: function(){
- //        $copy = $(this).clone();
- //        return $copy;
- //     },
-
- //        });
-
- //  $(".well").droppable({
- //    accept: '.item',
- //     drop: function(event, ui) {
- //       $(this).append($(ui.draggable).clone())
- //       $(".well .item").addClass("added_drop");
- //       $(".added").removeClass("item ui-draggable");
- //          $('.added').sortable();
-    
- //        // $(".added").draggable({
- //        //         // containment: 'parent',
- //        //         // grid: [150,150]
- //        //     });
-  
-       
- //            // dropped = true;
- //            // $.ui.ddmanager.current.cancelHelperRemoval = true;
- //            // ui.helper.appendTo(this);
- //    }
- //  })
-
-    
- // });
-
-
-
-//   var div = $('#resizable');
-// div.resizable(
-//     {
-//         stop: function(event, ui)
-//         {                        
-//             var top = getTop(ui.helper);
-//             ui.helper.css('position', 'fixed');
-//             ui.helper.css('top', top+"px");            
-//         }        
-//     });
-//     div.draggable(
-//         {    grid: [10,10],
-//         stop: function(event, ui)
-//         {            
-//             var top = getTop(ui.helper);
-           
-//         }
-//     });
-//     function getTop(ele)
-//     {
-//         var eTop = ele.offset().top;
-//         var wTop = $(window).scrollTop();
-//         var top = eTop - wTop;
-        
-//         return top;    
-//     }
-         // $(":text").prop("disabled", true);
-        // $('.input').draggable({
-        //    revert: 'invalid',
-        //   helper: 'clone'
-        // });
-
- 
-        // $('.well').droppable({
-        //   accept: 'div',
-        //   drop: function(ev, ui) {
-        //     grid: [10, 10]
-        //     ui.helper.css('position', 'fixed');
-        //     var droppedItem = $(ui.draggable).clone();
-        //     $(droppedItem).children().addClass('added');
-        //     $(this).append(droppedItem);
-
-            // console.log(this)
-            // var sum = 0
-            // $('#grocery_list .item').each(function() {
-            //   sum += new Number($(this).find('td').last().text());
-            // });
-            // $('#grocery_list td').last().text((sum).toFixed(2));
-        //   }
-        // });
-
-
-        // $('#containter-left').bind('dblclick', function(e){
-        //   if ($(e.target).hasClass('added')){
-        //     $(e.target).remove();
-        //   }
-        //   // $(e.target).remove();
-        // });
-        // $('#drag_it').mouseup(function(){
-
-        // })
    
 
+  $('.field_type').draggable(draggableOptions(false));
 
+  $('.form_field_catcher').droppable({
+    accept: '.field_type',
+    drop: function(event, ui){
+      var newUiDraggable = ui.draggable.clone();
+
+      $(newUiDraggable).removeClass("field_type").addClass("added");
+      $(this).append(newUiDraggable);
+
+      // $(newUiDraggable).css("top", Math.floor(ui.position.top / 10) * 10);
+      // $(newUiDraggable).css("left", Math.floor(ui.position.left / 10) * 10);
+      $(newUiDraggable).draggable(draggableOptions(true));
+      $(newUiDraggable).dblclick(function(){
+        $(this).remove();
+      });
+    }
+  });
 });
+
+function draggableOptions(withinCatcher) {
+  var options = {
+    scroll: false,
+    grid: [1, 30],
+    snap: true,
+    snapTolerance: 1,
+    preventCollision: true,
+    obstacle: ".added"
+  };
+  if (!withinCatcher) {
+    options.revert = 'invalid';
+    options.helper = 'clone';
+  } else {
+    options.containment = '.form_field_catcher';
+  }
+  return options;
+}
