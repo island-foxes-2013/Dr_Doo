45f1d0ea502e1d3a94ec61e9745428d44528ddf4
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-08-20 14:11:20 -0700

    WIP: got drag and drop working somewhat sanely

--- app/assets/javascripts/form_bulder.js
+++ app/assets/javascripts/form_bulder.js
@@ -1,21 +1,45 @@
-$(function() {
-   
+function draggableOptions(withinCatcher) {
+  var options = {
+    scroll: false,
+    grid: [30, 30],
+    snap: true,
+    snapTolerance: 1,
+    preventCollision: true,
+    obstacle: ".added"
+  };
+  if (withinCatcher) {
+    options.containment = '.form_field_catcher';
+  } else {
+    options.revert = 'invalid';
+    options.helper = 'clone';
+  }
+  return options;
+}
+
+$(function() {   
 
   $('.field_type').draggable(draggableOptions(false));
 
   $('.form_field_catcher').droppable({
     accept: '.field_type',
     drop: function(event, ui){
-      var newUiDraggable = ui.draggable.clone();
+      var $newUiDraggable = ui.draggable.clone();
+      var position = {
+        left: Math.floor( (ui.position.left - this.offsetLeft) / 10 ) * 10,
+        top: Math.floor( (ui.position.top - this.offsetTop) / 10 ) * 10
+      };
 
-      $(newUiDraggable).removeClass("field_type").addClass("added");
-      $(this).append(newUiDraggable);
+      console.log('droppable position (relative to page):', {left: this.offsetLeft, top: this.offsetTop});
+      console.log('drop position (relative to page):', {left: ui.position.left, top: ui.position.top});
+      console.log('drop position (relative to droppable):', position);
 
-      $(newUiDraggable).css("top", Math.floor(ui.position.top / 10) * 10);
-      $(newUiDraggable).css("left", Math.floor(ui.position.left / 10) * 10);
-      $(newUiDraggable).draggable(draggableOptions(true));
+      $newUiDraggable.removeClass("field_type").addClass("added");
+      $(this).append($newUiDraggable);
+      $newUiDraggable.css(position);
+      console.log('drop position (relative to droppable):', {left: $newUiDraggable.css("left"), top: $newUiDraggable.css("top")});
+      $newUiDraggable.draggable(draggableOptions(true));
 
-      $(newUiDraggable).dblclick(function(){
+      $newUiDraggable.dblclick(function(){
         $(this).remove();
       });
     }
@@ -32,23 +56,5 @@ $(function() {
   //   // $(this).parent().find('.comment_append').last().append('<hr />');
   //   // $('.comment_form textarea').val('');
   // });
+
 });
-
-function draggableOptions(withinCatcher) {
-
-  var options = {
-    scroll: false,
-    grid: [1, 30],
-    snap: true,
-    snapTolerance: 1,
-    preventCollision: true,
-    obstacle: ".added"
-  };
-  if (!withinCatcher) {
-    options.revert = 'invalid';
-    options.helper = 'clone';
-  } else {
-    options.containment = '.form_field_catcher';
-  }
-  return options;
-}
